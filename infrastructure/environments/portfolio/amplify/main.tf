d290a73e132c71864d9a97743d158e2f7c3f8812
tqer39 <tqer39@gmail.com>
2024-04-14 14:32:36 +0900

    ドメイン名を更新し、証明書のモジュールを追加。

--- infrastructure/environments/portfolio/amplify/main.tf
+++ infrastructure/environments/portfolio/amplify/main.tf
@@ -1,5 +1,12 @@
 module "domain" {
   source = "../../../modules/domain"
 
-  domain_name = "tqer39.dev"
+  domain_name = "time-capsule.tqer39.dev"
+}
+
+module "certificate" {
+  source = "../../../modules/certificate"
+
+  domain_name = "time-capsule.tqer39.dev"
+  zone_id     = module.domain.zone_id
 }

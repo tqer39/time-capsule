c0b40e37268e0fc78748a5449cd5daeedc1fd02d
tqer39 <tqer39@gmail.com>
2024-04-14 11:00:12 +0900

    locals.tfを更新し、us-east-1リージョンを追加。

--- infrastructure/environments/prod/shared/locals.tf
+++ infrastructure/environments/prod/shared/locals.tf
@@ -7,6 +7,7 @@ locals {
   owner          = "tqer39"
   region = {
     apne1 = "ap-northeast-1"
+    use1  = "us-east-1"
   }
   repository = "time-capsule"
 }
@@ -23,5 +24,6 @@ locals {
   # tflint-ignore: terraform_unused_declarations
   tfstate_bucket_name = {
     apne1 = "terraform-tfstate-${local.owner}-${local.aws_account_id}-${local.region.apne1}"
+    use1  = "terraform-tfstate-${local.owner}-${local.aws_account_id}-${local.region.use1}"
   }
 }

ad8fdd37d6824060458647af9219d6d85e920f32
tqer39 <tqer39@gmail.com>
2025-04-24 03:42:03 +0900

    Terraformのバージョン要件を更新し、S3バックエンド設定を追加

--- infra/envs/dev/base/apne1/cognito/terraform.tf
+++ infra/envs/dev/base/apne1/cognito/terraform.tf
@@ -1,10 +1,17 @@
 terraform {
-  required_version = "= 1.11.4"
+  required_version = "1.11.4"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "= 5.95.0"
+      version = "5.95.0"
     }
+  }
+
+  backend "s3" {
+    bucket  = "terraform-tfstate-tqer39-072693953877-ap-northeast-1"
+    encrypt = true
+    key     = "time-capsule/terraform/envs/dev-base-apne1-cognito.tfstate"
+    region  = "ap-northeast-1"
   }
 }

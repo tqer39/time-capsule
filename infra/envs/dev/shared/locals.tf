b3d4ead45c3ed62659935f5bae7253be5d3678e4
tqer39 <tqer39@gmail.com>
2025-04-24 03:45:51 +0900

    ローカル変数にアプリ環境名を追加し、環境タグを更新

--- infra/envs/dev/shared/locals.tf
+++ infra/envs/dev/shared/locals.tf
@@ -1,6 +1,7 @@
 locals {
   aws_account_id = "072693953877"
   aws_env_name   = "portfolio"
+  app_env_name   = "dev"
   platform       = "aws"
   owner          = "tqer39"
   region = {
@@ -11,15 +12,10 @@ locals {
 
 locals {
   common_tags = {
-    "environment" = local.aws_env_name
+    "environment" = local.app_env_name
     "iac"         = "terraform"
     "owner"       = "${local.owner}+${local.platform}-${local.aws_env_name}@gmail.com"
     "product"     = "time-capsule"
     "repository"  = "${local.owner}/${local.repository}"
-  }
-
-  # tflint-ignore: terraform_unused_declarations
-  tfstate_bucket_name = {
-    apne1 = "terraform-tfstate-${local.owner}-${local.aws_account_id}-${local.region.apne1}"
   }
 }
